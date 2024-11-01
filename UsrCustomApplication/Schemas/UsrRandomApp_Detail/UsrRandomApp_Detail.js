c33a333e1e52e591f5022eb263136f061c3aab8c
Romadan Saputra <hi.madan27@gmail.com>
2024-11-01 09:42:03 +0700

    application package - freedom ui

---
+++ UsrCustomApplication/Schemas/UsrRandomApp_Detail/UsrRandomApp_Detail.js
@@ -0,0 +1,8 @@
+define('UsrRandomApp_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrRandomApp',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
